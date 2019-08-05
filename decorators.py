2f9af18a500d28a99a61f909d16e78200f5fd085
Wang Yuxiang <yuxiangwang0829@gmail.com>
2019-08-05 17:26:49 +0800

    update decorator_examples.py

--- decorators.py
+++ decorators.py
@@ -5,6 +5,8 @@ another function. All of this without altering the source code
 of the original function that passed in.
 '''
 
+import decorator_examples
+
 
 def decorator_function(original_function):
     def wrapper_function(*args, **kwargs):
@@ -22,7 +24,7 @@ class decorator_class(object):
         return self.original_function(*args, **kwargs)
 
 
-@decorator_function
+@decorator_class
 def display():
     '''
     This function has been decorated by function 'decorator_function', it's actually a
@@ -32,11 +34,19 @@ def display():
     print('display function ran')
 
 
-@decorator_class
+@decorator_examples.my_logger
+@decorator_examples.my_timer
+@decorator_function
 def display_info(name, age):
+    '''
+    If there are more than two decorators, then the bottom one will be executed first,
+    and then will execute each decorator one by one in the order from the bottom to the
+    top. In this example, 'decorator_function' will be executed first, and then will be
+    'decorator_examples.my_timer', and 'decorator_examples.my_logger' will be the last.
+    '''
     print('display_info ran with arguments ({}, {})'.format(name, age))
 
 
-display_info('John', 25)
+display_info('Jack', 25)
 
 display()
